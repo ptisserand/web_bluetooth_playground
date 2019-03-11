120b1ef1afb341e0d070893a2a830165db8ea39d
Patrice Tisserand <p.tisserand@softnautic.com>
2019-03-11 15:33:03 +0100

    nrfconnect: iOs and android nRFConnect log files format are different....

--- nrfconnect.js
+++ nrfconnect.js
@@ -9,7 +9,7 @@ function convert_nrfconnect_time(time_str) {
     return ret;
 }
 
-function convert_nrfconnect_frame(frame_str) {
+function android_convert_nrfconnect_frame(frame_str) {
     let ret = [];
     frame_str.match(/[0-9A-F]{2}/g).forEach(dd => {
         ret.push(parseInt(dd, 16));
@@ -17,9 +17,21 @@ function convert_nrfconnect_frame(frame_str) {
     return new Uint8Array(ret);
 }
 
-
-function extract_frame_data(log_line) {
+function android_extract_frame_data(log_line) {
     let parsed = log_line.match(/I\s+([0-9]+:[0-9]+:[0-9]+\.[0-9]+).* value: \(0x\) (.*)/);
+    return {'time_str': parsed[1], 'frame_str': parsed[2]};
+}
+
+function ios_convert_nrfconnect_frame(frame_str) {
+    let ret = [];
+    frame_str.match(/[0-9A-F]{2}/g).forEach(dd => {
+        ret.push(parseInt(dd, 16));
+    });
+    return new Uint8Array(ret);
+}
+
+function ios_extract_frame_data(log_line) {
+    let parsed = log_line.match(/([0-9]+:[0-9]+:[0-9]+\.[0-9]+) 0x([0-9A-F]+) received/);
     return {'time_str': parsed[1], 'frame_str': parsed[2]};
 }
 
@@ -27,15 +39,32 @@ function convert_nrfconnect_log(full_log) {
     // filter line only containing notification received
     let ret = [];
     let notification_logs = full_log.match(/.*Notification received.*/g);
-    let start = extract_frame_data(notification_logs[0]);
-    let start_time = convert_nrfconnect_time(start.time_str);
-    console.log("Start time: " + start_time);
-    notification_logs.forEach(ll => {
-        let frame = extract_frame_data(ll);
-        let received_time = convert_nrfconnect_time(frame.time_str) - start_time;
-        let values = convert_nrfconnect_frame(frame.frame_str).buffer;
-        let atime = extractTimeFromFrame(values);
-        ret.push({'received_time': received_time, 'time': atime, 'data': values});
-    });
+    if ((null != notification_logs) && (notification_logs.length != 0)) {
+        let start = android_extract_frame_data(notification_logs[0]);
+        let start_time = convert_nrfconnect_time(start.time_str);
+        console.log("Android Start time: " + start_time);
+        notification_logs.forEach(ll => {
+            let frame = android_extract_frame_data(ll);
+            let received_time = convert_nrfconnect_time(frame.time_str) - start_time;
+            let values = android_convert_nrfconnect_frame(frame.frame_str).buffer;
+            let atime = extractTimeFromFrame(values);
+            ret.push({'received_time': received_time, 'time': atime, 'data': values});
+        });
+        return ret;
+    }
+    notification_logs = full_log.match(/.* 0x[0-9A-F]{46,} received/g);
+    if (notification_logs.length != 0) {
+        let start = ios_extract_frame_data(notification_logs[0]);
+        let start_time = convert_nrfconnect_time(start.time_str);
+        console.log("iOs Start time:" + start_time);
+        notification_logs.forEach(ll => {
+            let frame = ios_extract_frame_data(ll);
+            let received_time = convert_nrfconnect_time(frame.time_str) - start_time;
+            let values = ios_convert_nrfconnect_frame(frame.frame_str).buffer;
+            let atime = extractTimeFromFrame(values);
+            ret.push({'receved_time': received_time, 'time': atime, 'data': values});
+        });
+        return ret;
+    }
     return ret;
 }
